03fbbf1daa4d6c271150a4f6c70852e81f878b47
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2019-03-01 19:14:18 +0100

    converted and copied overs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # apertium-olo
-Apertium language data for Livvi (Olonets Karelian); Experimental, for real analysers refer to giella-olo at
+
+Apertium language data for Livvi (Olonets Karelian)
+
+Experimental, for real analysers refer to giella-olo at
+http://divvun.no/doc/lang/olo/olo.html (these are converted from that and
+additions are not good for general use).
---
+++ dev/giella2apes.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if test $# -lt 1 ; then
+    echo Usage: $0 giella-olo-dir
+    exit 2
+fi
+
+cat $1/src/morphology/root.lexc \
+    $1/src/morphology/stems/*.lexc \
+    $1/src/morphology/affixes/*.lexc \
+    $1/src/morphology/compounding.lexc \
+    $1/src/morphology/generated_files/*.lexc |\
+    sed -f dev/giella2apes.sed > apertium-olo.olo.lexc
+cp $1/src/phonology/olo-phon.twolc apertium-olo.olo.twolc
+
