bf93308474e9c4d09cd91d94fcf5321c70407d80
Flammie Pirinen <flammie@iki.fi>
2019-03-07 03:22:23 +0100

    test new treebankings

--- dev/puupankkiiri.bash
+++ dev/puupankkiiri.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LEGPARSER=$HOME/github/flammie/leg-ape-ud/legproc.py
+GTDESC=$HOME/gtsvn/langs/olo/src/analyser-gt-desc.hfstol
 file_id_diz=$(basename $@ | sed -e 's/.text//')
-cat $@ | egrep -v '^#!' | apertium -d . krl-morph > $file_id_diz
-python $LEGPARSER --not-rules apertium-krl.krl.leg -i $file_id_diz
+cat $@ | egrep -v '^#!' | hfst-proc $GTDESC > $file_id_diz
+python $LEGPARSER --giella --not-rules apertium-olo.olo.leg -i $file_id_diz
