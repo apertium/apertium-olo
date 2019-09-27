97661be4917f4e0471a20e0c9e081eb667312864
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2019-09-27 18:43:13 +0200

    debug

--- dev/puupankkiiri.bash
+++ dev/puupankkiiri.bash
@@ -3,5 +3,7 @@
 LEGPARSER=$HOME/github/flammie/leg-ape-ud/legproc.py
 GTDESC=$HOME/gtsvn/langs/olo/src/analyser-gt-desc.hfstol
 file_id_diz=$(basename $@ | sed -e 's/.text//')
-cat $@ | egrep -v '^#!' | hfst-proc $GTDESC > $file_id_diz
-python $LEGPARSER --giella --not-rules apertium-olo.olo.leg -i $file_id_diz
+cat $@ | egrep -v '^#!' | hfst-proc $GTDESC |\
+    sed -e 's/Sem\//Sem_/g' -e 's/Der\//Der_/g'\
+        -e 's/Err\//Err_/g' > $file_id_diz
+python $LEGPARSER --giella --debug --not-rules apertium-olo.olo.leg -i $file_id_diz
